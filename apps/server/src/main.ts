143b1b57d2eea9c70147239323f1c64440c8aa9e
mkluszczynski <mkluszczynski07@gmail.com>
2025-02-08 04:36:40 +0100

    Integrate round and category services into the main server; update game and category controllers for socket connections

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -5,17 +5,25 @@ import { playerController } from "./player/player-controller";
 import { gameController } from "./game/game-controller";
 import { teamController } from "./team/team-controller";
 import { TeamService } from "./team/team-service";
+import { RoundService } from "./round/round-service";
+import { CategoryService } from "./category/category-service";
+import { CategoryRepository } from "./category/category-repository";
+import { categoryController } from "./category/category-controller";
 
 const server = new Server(3000);
 
 const playerService = new PlayerService();
-const gameService = new GameService(playerService);
+const roundService = new RoundService();
 const teamService = new TeamService();
+const gameService = new GameService(playerService);
+const categoryRepository = new CategoryRepository("categories.json");
+const categoryService = new CategoryService(categoryRepository);
 
 const onConnection = (socket: Socket) => {
   playerController(socket, playerService);
-  gameController(socket, gameService);
+  gameController(socket, gameService, roundService, categoryService);
   teamController(socket, gameService, playerService, teamService);
+  categoryController(socket, categoryService);
 };
 
 server.on("connection", (socket) => {
